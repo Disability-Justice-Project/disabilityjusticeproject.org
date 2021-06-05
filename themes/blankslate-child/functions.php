affff21f393317a27d8c3fa6a896bc3c0fe2693a
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-06-05 15:43:35 -0400

    Set up child theme (#2)
    * Add Advanced Custom Fields
    
    * Add blankslate theme
    
    * Add blankslate-child

---
+++ themes/blankslate-child/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Disability Justice Project functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Disability_Justice_Project
+ */
+
