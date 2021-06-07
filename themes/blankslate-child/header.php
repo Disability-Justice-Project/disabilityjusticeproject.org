ee6cc32e5af459104cb25f84d7e7550fe000f592
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-06-07 00:06:45 -0400

    Style header and footer (#5)
    * Add header layout
    
    * Remove boilerplate main-navigation class
    
    * Add primary nav component
    
    * Add thick border custom property
    
    * Re-add project tease example
    
    * Add header
    
    * Update font weights
    
    * Add weight utility classes
    
    * Add test color utility classes
    
    * Add breakpoint variables
    
    * Add footer styling
    
    * Tweak tease styling
    
    * Tweak button styling
    
    * Add update banner
    
    * Style imports
    
    * Add update banner, footer
    
    * Update CSS

---
+++ themes/blankslate-child/header.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Disability_Justice_Project
+ */
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'disabilityjusticeproject' ); ?></a>
+
+<header
+  id="masthead"
+  class="l-header">
+  <?php
+    the_custom_logo();
+    if ( is_front_page() && is_home() ) :
+    ?>
+      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+    <?php
+    else :
+    ?>
+      <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+    <?php
+  endif; ?>
+
+
+  <nav
+    id="primary-navigation"
+    class="main-navigation c-nav-primary"
+    aria-label="Primary">
+    <button
+      class="menu-toggle"
+      aria-controls="primary-menu"
+      aria-expanded="false">
+        <?php esc_html_e( 'Primary Menu', 'blankslate-child' ); ?>
+    </button>
+    <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+  </nav><!-- #site-navigation -->
+</header><!-- #masthead -->
