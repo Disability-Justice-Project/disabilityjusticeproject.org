b4e87b13003276f40b9f49824ca0f4b7c83061b6
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-06-07 00:45:03 -0400

    Restrict editor blocks (#7)

--- themes/blankslate-child/functions.php
+++ themes/blankslate-child/functions.php
@@ -7,3 +7,22 @@
  * @package Disability_Justice_Project
  */
 
+// Restrict blocks available to site authors
+function my_plugin_allowed_block_types( $allowed_block_types, $post ) {
+  if ( $post->post_type !== 'post' ) {
+    return $allowed_block_types;
+  }
+  return array(
+    'core/paragraph',
+    'core/heading',
+    'core/list',
+    'core/quote',
+    'core/image',
+    'core/audio',
+    'core/video',
+    'core/image',
+    'core/shortcode'
+  );
+}
+add_filter( 'allowed_block_types', 'my_plugin_allowed_block_types', 10, 2 );
+?>
