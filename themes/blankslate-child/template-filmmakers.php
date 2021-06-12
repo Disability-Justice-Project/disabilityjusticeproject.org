1bd73b9560b68fe898aca5af3584064f2adf2840
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-06-12 17:24:06 -0400

    Add filmmakers page (#13)
    * Fix root sizing Custom Properties
    
    * Add filmmaker component
    
    * Tweak mentor photo layout
    
    * Add medium heading
    
    * Add email share icon
    
    * Add dark-on-light modifier
    
    * Upload filmmaker
    
    * Add initial static filmmakers content
    
    * Update CSS

---
+++ themes/blankslate-child/template-filmmakers.php
@@ -0,0 +1,53 @@
+<?php
+  /*
+  Template Name: Filmmakers Template
+  Template Post Type: page
+  */
+
+  get_header();
+?>
+
+<main id="content">
+  <div class="l-about l-about--dark-on-light">
+    <div class="l-about__content">
+
+      <h1 id="title" class="c-heading__large">
+        <?php the_title(); ?>
+      </h1>
+
+      <div class="">
+        <div class="c-filmmaker">
+          <img
+            alt="Photo of Name Here."
+            class="c-filmmaker__photo"
+            src="https://raw.githubusercontent.com/Disability-Justice-Project/disabilityjusticeproject.org/main/uploads/2021/05/andrew-pulrang.jpg" />
+          <div class="c-filmmaker__content">
+            <h3 class="c-heading__medium c-filmmaker__name">
+              Name Here
+            </h3>
+            <p class="c-filmmaker__bio">
+              Snout snout sodium for the ensnare bosom of the genus pathos and missing. Tundra tundra tocsin for the nutmeg isotope of the peasant ingot and ottoman. Uncle uncle udder for the dunes cloud of the hindu thou and continuum. Vulcan vulcan vocal for the alluvial ovoid of the yugoslav chekhov and revved. Whale whale woman for the meanwhile blowout of the forepaw meadow and glowworm.
+            </p>
+          </div>
+        </div>
+        <div class="c-filmmaker">
+          <img
+            alt="Photo of Name Here."
+            class="c-filmmaker__photo"
+            src="https://raw.githubusercontent.com/Disability-Justice-Project/disabilityjusticeproject.org/main/uploads/2021/05/andrew-pulrang.jpg" />
+          <div class="c-filmmaker__content">
+            <h3 class="c-heading__medium c-filmmaker__name">
+              Name Here
+            </h3>
+            <p class="c-filmmaker__bio">
+              Snout snout sodium for the ensnare bosom of the genus pathos and missing. Tundra tundra tocsin for the nutmeg isotope of the peasant ingot and ottoman. Uncle uncle udder for the dunes cloud of the hindu thou and continuum. Vulcan vulcan vocal for the alluvial ovoid of the yugoslav chekhov and revved. Whale whale woman for the meanwhile blowout of the forepaw meadow and glowworm.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
