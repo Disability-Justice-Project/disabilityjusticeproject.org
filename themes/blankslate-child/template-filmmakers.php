9b5179b6c2d4d520f124d31b58738db50daff6ca
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-06-21 19:28:24 -0400

    Add landing content templates (#17)
    * Add dark on light utility style
    
    * Add content topic, tertiary font for content copy
    
    * Add tracking custom properties
    
    * Thread dark-on-light utility class into components
    
    * Update mentor style to use grid
    
    * Add landing layout styles
    
    * Update about to use landing layout styles
    
    * Import partials
    
    * Update CSS
    
    * Rename mentor component to person
    
    * Add filmmaker and mentor components
    
    * Add title id
    
    * List filmmakers, use landing layout classes
    
    * List mentors, use dark-on-light utility class
    
    * Update site CSS

--- themes/blankslate-child/template-filmmakers.php
+++ themes/blankslate-child/template-filmmakers.php
@@ -8,13 +8,15 @@
 ?>
 
 <main id="content">
-  <div class="l-about l-about--dark-on-light">
-    <div class="l-about__content">
+  <div class="l-landing">
 
-      <h1 id="title" class="c-heading__large">
+    <div class="l-landing__title">
+      <h1 id="title" class="l-landing__heading c-heading__large">
         <?php the_title(); ?>
       </h1>
+    </div>
 
+    <div class="l-landing__person">
       <?php
         $args = array(
           'category_name' => 'filmmaker',
@@ -25,7 +27,7 @@
         $query = new WP_Query($args);
 
         while($query -> have_posts()) : $query -> the_post(); ?>
-          <?php get_template_part('tease-person'); ?>
+          <?php get_template_part('filmmaker'); ?>
       <?php endwhile; ?>
 
     </div>
