24eed58ba475e6a0e831ac5554a5a3b429c3e57b
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-06-21 21:04:59 -0400

    Add logo and favicons (#18)
    * Add support for uploaded SVGs
    
    * Add logos
    
    * Add social share and favicons
    
    * Add logo to header and footer, style
    
    * Add Bhargava headshot
    
    * Update CSS

--- themes/blankslate-child/footer.php
+++ themes/blankslate-child/footer.php
@@ -3,9 +3,13 @@
   id="footer"
   class="l-footer">
 
-  <p>
-    Logo TK
-  </p>
+  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+    <img
+      alt="Disability Justice Project."
+      role="img"
+      class="l-footer__logo"
+      src="/wp-content/uploads/2021/06/logo-light-on-dark.svg" />
+  </a>
 
   <?php
     $disabilityjusticeproject_description = get_bloginfo( 'description', 'display' );
--- themes/blankslate-child/header.php
+++ themes/blankslate-child/header.php
@@ -16,6 +16,22 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="icon" href="/wp-content/themes/blankslate-child/images/favicon.ico">
+  <link rel="icon" href="/wp-content/themes/blankslate-child/images/icon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/wp-content/themes/blankslate-child/images/apple-touch-icon.png">
+
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://disabilityjusticeproject.com/">
+  <meta property="og:title" content="Disability Justice Project">
+  <meta property="og:description" content="A global grassroots media network centering the voices of persons with disabilities.">
+  <meta property="og:image" content="/wp-content/themes/blankslate-child/images/share-image-facebook.png">
+
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://disabilityjusticeproject.com/">
+  <meta property="twitter:title" content="Disability Justice Project">
+  <meta property="twitter:description" content="A global grassroots media network centering the voices of persons with disabilities.">
+  <meta property="twitter:image" content="/wp-content/themes/blankslate-child/images/share-image-twitter.png">
+
   <?php wp_head(); ?>
 </head>
 
@@ -31,11 +47,22 @@
     the_custom_logo();
     if ( is_front_page() && is_home() ) :
     ?>
-      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+        <img
+          alt="Disability Justice Project."
+          role="img"
+          class="l-header__logo"
+          src="/wp-content/uploads/2021/06/logo-light-on-dark.svg" />
+      </a>
     <?php
     else :
     ?>
-      <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+        <img
+          alt="Disability Justice Project."
+          class="l-header__logo"
+          src="/wp-content/uploads/2021/06/logo-light-on-dark.svg" />
+      </a>
     <?php
   endif; ?>
 
