5e8cdbc035d95a4ec396eb371c2ca4460efed4af
Eric Bailey <eric.w.bailey@gmail.com>
2021-06-23 11:52:31 -0400

    Add transcript toggle controls

--- themes/blankslate-child/tease-project.php
+++ themes/blankslate-child/tease-project.php
@@ -33,8 +33,12 @@
       </div>
     </div>
     <div class="c-tease-project__tools">
-      <button type="button" class="c-tease-project__transcript">
-        Transcript
+      <button
+        data-transcript="button"
+        aria-pressed="false"
+        type="button"
+        class="c-tease-project__transcript">
+        Transcript<span class="screen-reader-text"> for <?php the_title(); ?></span>
       </button>
       <button type="button" class="c-share__facebook">
         <svg class="c-share__icon" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg>
