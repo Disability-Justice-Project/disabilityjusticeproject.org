ed2322e21e5d16652eb4c3ff4fc8528d64761e5e
Eric Bailey <eric.w.bailey@gmail.com>
2021-06-27 12:42:58 -0400

    Import logo partial

--- themes/blankslate-child/footer.php
+++ themes/blankslate-child/footer.php
@@ -4,11 +4,7 @@
   class="l-footer">
 
   <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-    <img
-      alt="Disability Justice Project."
-      role="img"
-      class="l-footer__logo"
-      src="/wp-content/uploads/2021/06/logo-light-on-dark.svg" />
+    <?php get_template_part('logo');?>
   </a>
 
   <?php
