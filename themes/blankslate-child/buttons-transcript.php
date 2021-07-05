24e1e58f10925d2ff61041c84d312711cff6d88e
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-05 15:06:43 -0400

    Break share and transcript buttons out into partials

---
+++ themes/blankslate-child/buttons-transcript.php
@@ -0,0 +1,7 @@
+<button
+  data-transcript="button"
+  aria-pressed="false"
+  type="button"
+  class="c-tease-project__transcript">
+  Transcript<span class="screen-reader-text"> for <?php the_title(); ?></span>
+</button>
