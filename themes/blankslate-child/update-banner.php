8905e1b632b7b0584abb82ebd6cf721f55a370ec
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-05 15:59:18 -0400

    Remove visually hidden colon and period

--- themes/blankslate-child/update-banner.php
+++ themes/blankslate-child/update-banner.php
@@ -1,7 +1,7 @@
 <?php if ( ! empty($new_video)) : ?>
   <div class="c-update-banner">
     <p id="title" class="c-update-banner__message">
-      Documentaries<span class="screen-reader-text">:</span> <span class="u-color-text-gray-brick"><?php echo $new_video; ?><span class="screen-reader-text">.</span></span>
+      Documentaries <span class="u-color-text-gray-brick"><?php echo $new_video; ?></span>
     </p>
   </div>
 <?php endif; ?>
