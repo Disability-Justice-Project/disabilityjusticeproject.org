4a58a53d56fb361ded2d0bbb28a1aeb5849bab70
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-05 21:54:43 -0400

    Add custom excerpt length function

--- themes/blankslate-child/functions.php
+++ themes/blankslate-child/functions.php
@@ -27,4 +27,19 @@ function my_plugin_allowed_block_types( $allowed_block_types, $post ) {
 }
 add_filter( 'allowed_block_types', 'my_plugin_allowed_block_types', 10, 2 );
 
+
+// Filter except length
+function excerpt($limit) {
+  $excerpt = explode(' ', get_the_excerpt(), $limit);
+  if (count($excerpt)>=$limit) {
+    array_pop($excerpt);
+    $excerpt = implode(" ",$excerpt).'';
+  } else {
+    $excerpt = implode(" ",$excerpt);
+  }
+  $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+  return $excerpt;
+}
+
+
 ?>
