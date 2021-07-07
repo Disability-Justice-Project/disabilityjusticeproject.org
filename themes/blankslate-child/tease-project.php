0798f4198f18cbcdc4fe37d3e0177f909c36714d
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-07 19:35:50 -0400

    Programatically communicate modal states

--- themes/blankslate-child/tease-project.php
+++ themes/blankslate-child/tease-project.php
@@ -51,10 +51,16 @@
   <div class="c-tease-project__accordion-wrapper">
     <section
       aria-hidden="true"
-      aria-label="Transcript for <?php the_title(); ?>"
+      aria-describedby="transcript-for-<?php echo sanitize_title(get_the_title()) ?>"
       class="c-tease-project__accordion-content u-flow"
-      data-transcript="content"
-      tabindex="0">
+      data-transcript="content">
+      <h4
+        id="transcript-for-<?php echo sanitize_title(get_the_title()) ?>"
+        data-transcript="title"
+        class="screen-reader-text"
+        tabindex="-1">
+        Transcript for <?php the_title(); ?>
+      </h4>
       <?php echo $transcript; ?>
     </section>
   </div>
