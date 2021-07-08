b25cd3f94ffb48f86a4f2a3531d4bb75d5d8c4b4
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-08 15:25:36 -0400

    Copy tweak

--- themes/blankslate-child/template-news.php
+++ themes/blankslate-child/template-news.php
@@ -64,7 +64,7 @@
   </div>
   <div class="l-tease-news">
     <h2 class="l-tease-news__title">
-      News <span class="u-color-text-gray-brick">From the Frontlines of Disability Justice</span>
+      News <span class="u-color-text-gray-brick">From the Global Frontlines of Disability Justice</span>
     </h2>
     <div class="l-tease-news__posts">
       <?php
