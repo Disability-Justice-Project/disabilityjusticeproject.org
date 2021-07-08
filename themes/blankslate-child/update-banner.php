46575f6449056a47e3e629a9da90615d8c61afdf
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-08 14:45:24 -0400

    Update homepage title styling

--- themes/blankslate-child/update-banner.php
+++ themes/blankslate-child/update-banner.php
@@ -1,7 +1,7 @@
 <?php if ( ! empty($new_video)) : ?>
   <div class="c-update-banner">
     <h1 id="title" class="c-update-banner__message">
-      Documentaries <span class="u-color-text-gray-brick"><?php echo $new_video; ?></span>
+      Documentaries <span class="u-color-text-gray-gold">by</span> and <span class="u-color-text-gray-gold">about</span> persons with disabilities
     </h1>
   </div>
 <?php endif; ?>
