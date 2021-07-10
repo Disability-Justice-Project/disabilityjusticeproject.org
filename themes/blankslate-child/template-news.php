bb043053fb2e8f7f0b8dbec083e8c01a08140ad8
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-10 14:23:14 -0400

    Turn recent news into a component partial

--- themes/blankslate-child/template-news.php
+++ themes/blankslate-child/template-news.php
@@ -62,24 +62,7 @@
 
 
   </div>
-  <div class="l-tease-news">
-    <h2 class="l-tease-news__title">
-      News <span class="u-color-text-gray-brick">From the Global Frontlines of Disability Justice</span>
-    </h2>
-    <div class="l-tease-news__posts">
-      <?php
-        $args = array(
-          'category_name' => 'news',
-          'posts_per_page' => 3,
-          'no_found_rows' => true
-        );
-        $query = new WP_Query($args);
-
-        while($query -> have_posts()) : $query -> the_post(); ?>
-          <?php get_template_part('tease-news'); ?>
-      <?php endwhile; ?>
-    </div>
-  </div>
+  <?php get_template_part('recent-news');?>
 </main>
 
 <?php get_footer(); ?>
