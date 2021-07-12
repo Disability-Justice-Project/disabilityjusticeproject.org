f7019b20c502074f7a74dda54b570d73bdaae5e5
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-07-11 20:48:34 -0400

    Initial dynamic accessibility settings work (#52)
    * Add value properties
    
    * Toggle body classes for settings radio buttons

--- themes/blankslate-child/js/main.js
+++ themes/blankslate-child/js/main.js
@@ -1,25 +1,5 @@
 (function($) {
 
-  // Toggle accessibility settings modal
-  var accessibilitySettingsToggle = $('#accessibility-settings-toggle');
-  var accessibilitySettingsModal = $('#accessibility-settings-modal').hide();
-  var accessibilitySettingsModalTitle = $('#accessibility-settings-modal-title');
-
-  accessibilitySettingsToggle.click(function(event) {
-    if ( accessibilitySettingsModal.attr('hidden') ) {
-      accessibilitySettingsToggle.attr("aria-expanded", "true");
-      accessibilitySettingsModal.removeAttr('hidden');
-      accessibilitySettingsModal.slideDown();
-      accessibilitySettingsModalTitle.focus();
-    } else {
-      accessibilitySettingsModal.prop('hidden', true);
-      accessibilitySettingsModal.hide();
-      accessibilitySettingsToggle.attr("aria-expanded", "false");
-      accessibilitySettingsToggle.focus();
-    }
-    return false;
-  });
-
   // Toggle transcripts
   var transcriptButtons = $('[data-transcript="button"]');
   var transcriptTitle = $('[data-transcript="title"]');
@@ -42,10 +22,84 @@
     return false;
   });
 
+
+
+  // Toggle accessibility settings modal
+  var accessibilitySettingsToggle = $('#accessibility-settings-toggle');
+  var accessibilitySettingsModal = $('#accessibility-settings-modal').hide();
+  var accessibilitySettingsModalTitle = $('#accessibility-settings-modal-title');
+
+  accessibilitySettingsToggle.click(function(event) {
+    if ( accessibilitySettingsModal.attr('hidden') ) {
+      accessibilitySettingsToggle.attr("aria-expanded", "true");
+      accessibilitySettingsModal.removeAttr('hidden');
+      accessibilitySettingsModal.slideDown();
+      accessibilitySettingsModalTitle.focus();
+    } else {
+      accessibilitySettingsModal.prop('hidden', true);
+      accessibilitySettingsModal.hide();
+      accessibilitySettingsToggle.attr("aria-expanded", "false");
+      accessibilitySettingsToggle.focus();
+    }
+    return false;
+  });
+
   var accessibilitySettingsCloseButton = $('[data-modal-close-button]');
   accessibilitySettingsCloseButton.click(function(event) {
     accessibilitySettingsModal.prop('hidden', true);
     return false;
   });
+
+
+  // Check and set defaults
+  var fontSizeDefault = $('#font-size-default').prop("checked", true);
+  var lineHeightDefault = $('#line-height-default').prop("checked", true);
+  var contrastDefault = $('#contrast-default').prop("checked", true);
+  var noHighlight = $('#no-highlight').prop("checked", true);
+  $(document.body).addClass('js-font-size-default js-line-height-default js-contrast-default');
+
+
+  // Toggle font size settings
+  $('input[name="font-size"]').change(function(){
+    var inputVal = $('input[name="font-size"]:checked').val();
+    $(document.body)
+      .removeClass('js-font-size-default js-font-size-large js-font-size-extra-large')
+      .addClass("js-" + inputVal);
+  });
+
+  // Toggle line height settings
+  $('input[name="line-height"]').change(function(){
+    var inputVal = $('input[name="line-height"]:checked').val();
+    $(document.body)
+      .removeClass('js-line-height-default js-line-height-more js-line-height-max')
+      .addClass("js-" + inputVal);
+  });
+
+
+  // Toggle contrast settings
+  $('input[name="contrast"]').change(function(){
+    var inputVal = $('input[name="contrast"]:checked').val();
+    $(document.body)
+      .removeClass('js-contrast-default js-contrast-yellow js-contrast-white js-contrast-blue')
+      .addClass("js-" + inputVal);
+  });
+
+  // Toggle highlight style settings
+  $('input[name="highlight-style"]').change(function(){
+    var inputVal = $('input[name="highlight-style"]:checked').val();
+    $(document.body)
+      .removeClass('js-no-highlight js-highlighted')
+      .addClass("js-" + inputVal);
+  });
+
+  // Restores default accessibility modal settings
+  var accessibilitySettingsResetButton = $('[data-modal-button-reset]');
+  accessibilitySettingsResetButton.click(function(event) {
+    $(document.body)
+      .removeClass('js-font-size-default js-font-size-large js-font-size-extra-large js-line-height-default js-line-height-more js-line-height-max js-contrast-default js-contrast-yellow js-contrast-white js-contrast-blue js-no-highlight js-highlighted')
+      .addClass('js-font-size-default js-line-height-default js-contrast-default js-no-highlight');
+    return false;
+  });
+
 
 })(jQuery);
