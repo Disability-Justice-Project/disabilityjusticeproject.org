6efbe955950d73063648705e19e621bd63c5b0db
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-12 20:21:27 -0400

    Add film category template

---
+++ themes/blankslate-child/category-film.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* The Film Archive Template
+*/
+
+get_header(); ?>
+
+<main id="content">
+
+  <h1 id="title" class="screen-reader-text">
+    <?php
+      $category = get_the_category();
+      echo $category[0]->cat_name;
+    ?>
+  </h1>
+
+  <?php
+    $args = array(
+      'category_name' => 'film',
+      'no_found_rows' => true
+    );
+    $query = new WP_Query($args);
+
+    while($query -> have_posts()) : $query -> the_post(); ?>
+      <?php get_template_part('tease-project'); ?>
+  <?php endwhile; ?>
+
+</main>
+
+<?php get_footer(); ?>
