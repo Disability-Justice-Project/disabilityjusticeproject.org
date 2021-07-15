908c1554ceb2f6a0667b58323273ad63a00cb434
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-14 21:43:09 -0400

    Style white highlight

--- themes/blankslate-child/update-banner.php
+++ themes/blankslate-child/update-banner.php
@@ -1,7 +1,7 @@
 <?php if ( ! empty($new_video)) : ?>
   <div class="c-update-banner">
     <h1 id="title" class="c-update-banner__message">
-      Documentaries <span class="u-color-text-gray-gold">by</span> and <span class="u-color-text-gray-gold">about</span> persons with disabilities
+      Documentaries <span class="u-color-text-gold">by</span> and <span class="u-color-text-gold">about</span> persons with disabilities
     </h1>
   </div>
 <?php endif; ?>
