58bb8fd49e2e8aa1209f06691788497efe8a1a87
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-18 13:26:33 -0400

    Order last 10 videos randomly

---
+++ themes/blankslate-child/recommended-video.php
@@ -0,0 +1,24 @@
+<?php
+  $page_slug = $post->post_name;
+  $transcript = get_field('transcript');
+  $video_id = get_field('video_id');
+?>
+
+<div class="c-recommended-video">
+  <div class="u-embed-responsive" onclick="location.href='<?php the_permalink(); ?>'">
+    <img
+      alt="Video thumbnail for <?php the_title(); ?>."
+      class="u-effect-gold-screen c-recommended-video__thumbnail"
+      src="https://img.youtube.com/vi/<?php echo $video_id; ?>/mqdefault.jpg">
+  </div>
+  <h3 class="c-recommended-video__title">
+    <a
+      class="c-recommended-video__link"
+      href="<?php the_permalink(); ?>">
+      <?php the_title(); ?>
+    </a>
+  </h3>
+  <p class="c-recommended-video__excerpt">
+    <span class="c-recommended-video__about-this-video">About this video:</span> <?php echo excerpt(55); ?>
+  </p>
+</div>
