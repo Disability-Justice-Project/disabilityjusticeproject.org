de2ba36a12718181ea68efe8e5ca619e59d6df84
Eric Bailey <eric.w.bailey@gmail.com>
2021-07-31 18:02:11 -0400

    Initial grid work

--- themes/blankslate-child/template-homepage.php
+++ themes/blankslate-child/template-homepage.php
@@ -4,33 +4,32 @@
   Template Post Type: page
   */
 
-  // Get homepage template data
-  $new_video = get_post_meta( get_the_ID(), 'new_video', true);
-
   get_header();
 ?>
 
 <main id="content">
 
-<?php
-  set_query_var('new_video', $new_video);
-  get_template_part('update-banner');
-?>
+  <div class="l-grid">
+    <div class="c-update-banner">
+      <h1 id="title" class="c-update-banner__message">
+        Documentaries <span class="u-color-text-gold">by</span> and <span class="u-color-text-gold">about</span> persons with disabilities
+      </h1>
+    </div>
 
-<?php
-  $args = array(
-    'category_name' => 'film',
-    'posts_per_page' => 3,
-    'no_found_rows' => true
-  );
-  $query = new WP_Query($args);
+    <?php
+      $args = array(
+        'category_name' => 'film',
+        'posts_per_page' => 3,
+        'no_found_rows' => true
+      );
+      $query = new WP_Query($args);
 
-  while($query -> have_posts()) : $query -> the_post(); ?>
-    <?php get_template_part('tease-project'); ?>
-<?php endwhile; ?>
+      while($query -> have_posts()) : $query -> the_post(); ?>
+        <?php get_template_part('tease-project'); ?>
+    <?php endwhile; ?>
+  </div>
 
-<?php get_template_part('recent-news');?>
-
+  <?php get_template_part('recent-news');?>
 </main>
 
 <?php get_footer(); ?>
