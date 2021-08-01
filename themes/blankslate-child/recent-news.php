9dbb6b6befe92199cdd59eed765404eaa5d6fe00
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-08-01 16:02:04 -0400

    Address accessibility settings feedback (#72)

--- themes/blankslate-child/recent-news.php
+++ themes/blankslate-child/recent-news.php
@@ -1,6 +1,6 @@
 <div id="news" class="l-tease-news">
   <h2 class="l-tease-news__title">
-    News <span class="u-color-text-brick">From the Global Frontlines of Disability Justice</span>
+    News <span class="l-tease-news__emphasis">From the Global Frontlines of Disability Justice</span>
   </h2>
   <div class="l-tease-news__posts">
     <?php
