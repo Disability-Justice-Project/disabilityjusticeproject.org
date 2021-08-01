5ce3067ca5edbb13a4b823f9d41751518bf8f9b5
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-08-01 12:15:01 -0400

    Add Disability Rights Fund logo (#71)
    * Add partership component
    
    * Align footer to grid
    
    * Add Disability Rights Fund logo
    
    * List Disability Rights Fund partnership
    
    * Update CSS

--- themes/blankslate-child/footer.php
+++ themes/blankslate-child/footer.php
@@ -11,7 +11,20 @@
     $disabilityjusticeproject_description = get_bloginfo( 'description', 'display' );
     if ( $disabilityjusticeproject_description || is_customize_preview() ) :
     ?>
+    <div class="l-footer__partnership c-partnership">
       <p class="c-footer__tagline"><?php echo $disabilityjusticeproject_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+      <div class="c-partnership__lockup">
+        <p class="c-partnership__byline">
+          A strategic partnership with
+        </p>
+        <a href="https://disabilityrightsfund.org/">
+          <img
+            alt="Disability Rights Fund."
+            class="c-partnership__logo"
+            src="/wp-content/themes/blankslate-child/images/logo-disability-rights-fund.png" />
+        </a>
+      </div>
+    </div>
   <?php endif; ?>
 
 </footer>
