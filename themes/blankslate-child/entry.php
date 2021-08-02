7e66e7118ee5a13e31b0911e7a60cb7896ff6d28
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-08-01 20:32:37 -0400

    Style skiplink (#73)
    * Add skipnav component
    
    * Update skiplink targets
    
    * Give partnership link hover/focus treatment
    
    * Update mobile grid
    
    * Update CSS

--- themes/blankslate-child/entry.php
+++ themes/blankslate-child/entry.php
@@ -5,7 +5,7 @@
 
 <div id="post-<?php the_ID(); ?>" class="l-landing">
 
-  <div class="l-landing__hero">
+  <div id="title" class="l-landing__hero">
     <div class="u-embed-responsive">
       <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/<?php echo $video_id; ?>" title="YouTube: <?php the_title(); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -14,7 +14,7 @@
   <p class="l-landing__topic c-content__topic">
     Film
   </p>
-  <h1 id="title" class="l-landing__heading c-heading__large">
+  <h1 class="l-landing__heading c-heading__large">
     <?php the_title(); ?>
   </h1>
 
