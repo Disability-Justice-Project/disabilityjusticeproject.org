14ead8580852a0e080277bc983ddd5b38721470b
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-08-17 21:55:13 -0400

    Add newsletter signup component (#89)
    * Add forms mixin logic and newsletter signup layout
    
    * Remove bottom padding
    
    * Add newsletter signup component
    
    * Include newsletter signup componetn on homepage
    
    * Style newsletter signup component
    
    * Update CSS

---
+++ themes/blankslate-child/newsletter-signup.php
@@ -0,0 +1,12 @@
+<aside id="newsletter-signup" class="l-newsletter-signup">
+  <div class="l-newsletter-signup__wrapper">
+    <form class="c-newsletter-signup">
+      <legend class="sr-only">
+        Newsletter signup
+      </legend>
+      <label class="c-newsletter-signup__label" for="your-email">Stay up to date on the global disability justice movement</label>
+      <input placeholder="* Your email is required" class="c-newsletter-signup__input" id="your-email" name="your-email" autocomplete="email" autocapitalize="off" autocorrect="off" spellcheck="false" type="email">
+      <input class="c-newsletter-signup__submit" type="submit" value="Submit">
+    </form>
+  </div>
+</aside>
