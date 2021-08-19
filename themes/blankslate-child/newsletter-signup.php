6e781dc03de1f32802b58132e074aad82913ea17
Eric Bailey <ericwbailey@users.noreply.github.com>
2021-08-18 23:57:48 -0400

    Integrate TinyLetter (#91)
    * Integrate TinyLetter code
    
    * Update newsletter styling

--- themes/blankslate-child/newsletter-signup.php
+++ themes/blankslate-child/newsletter-signup.php
@@ -1,12 +1,17 @@
 <aside id="newsletter-signup" class="l-newsletter-signup">
   <div class="l-newsletter-signup__wrapper">
-    <form class="c-newsletter-signup">
+    <!-- TinyLetter signup form -->
+    <form class="c-newsletter-signup" action="https://tinyletter.com/disabilityjustice" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/disabilityjustice', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
       <legend class="sr-only">
         Newsletter signup
       </legend>
-      <label class="c-newsletter-signup__label" for="your-email">Stay up to date on the global disability justice movement</label>
-      <input placeholder="* Your email is required" class="c-newsletter-signup__input" id="your-email" name="your-email" autocomplete="email" autocapitalize="off" autocorrect="off" spellcheck="false" type="email">
-      <input class="c-newsletter-signup__submit" type="submit" value="Submit">
+      <label class="c-newsletter-signup__label" for="tlemail">Stay up to date on the global disability justice movement</label>
+      <div class="c-newsletter-signup__input-wrapper">
+        <input class="c-newsletter-signup__input" name="email" id="tlemail" autocomplete="email" autocapitalize="off" autocorrect="off" spellcheck="false" type="email" />
+        <p class="c-newsletter-signup__instructions">Your email is required</p>
+      </div>
+      <input type="hidden" value="1" name="embed"/>
+      <input class="c-newsletter-signup__submit" type="submit" value="Subscribe" />
     </form>
   </div>
 </aside>
