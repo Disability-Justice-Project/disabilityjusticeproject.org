473c775b4f51385e493651dda575afc240da92d4
Eric Bailey <eric.w.bailey@gmail.com>
2021-08-24 22:22:22 -0400

    Add mentor title

--- themes/blankslate-child/staff.php
+++ themes/blankslate-child/staff.php
@@ -1,9 +1,18 @@
+<?php
+  $staff_title = get_field('staff_title');
+?>
+
 <?php if ( has_post_thumbnail() ) : ?>
   <?php the_post_thumbnail('full', array('class' => 'c-mentor__photo')); ?>
 <?php endif; ?>
 <div class="c-mentor__content">
   <h3 class="c-mentor__name">
     <?php the_title(); ?>
+    <?php if ( ! empty($staff_title)) : ?>
+      <span class="c-mentor__title">
+        <?php echo $staff_title; ?>
+      </span>
+    <?php endif; ?>
   </h3>
   <div class="c-mentor__bio">
     <?php the_content(); ?>
