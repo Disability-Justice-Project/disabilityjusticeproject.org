97b2ff1e8242056ed3c0425fb0384527c11dc21d
Eric Bailey <eric.w.bailey@gmail.com>
2021-08-24 23:27:32 -0400

    Add table of contents

--- themes/blankslate-child/template-about.php
+++ themes/blankslate-child/template-about.php
@@ -13,7 +13,7 @@
 
     <div class="l-landing__hero"></div>
 
-    <p class="l-landing__topic c-content__topic">
+    <p id="faq" class="l-landing__topic c-content__topic">
       FAQ
     </p>
     <h1 id="title" class="l-landing__heading c-heading__large">
@@ -22,11 +22,27 @@
 
     <div class="l-landing__content">
       <div class="c-content">
+        <nav class="c-content__toc" aria-label="Table of contents">
+          <ul>
+            <li><a href="#what-is-the-disability-justice-project">What is the Disability Justice Project?</a></li>
+            <li><a href="#what-is-participatory-media">What is participatory media?</a></li>
+            <li><a href="#what-is-the-disability-justice-project-fellowship">What is the Disability Justice Project Fellowship?</a>
+              <ul>
+                <li><a href="#who-can-apply-to-djp">Who can apply to DJP?</a></li>
+                <li><a href="#what-is-required-of-the-fellows-and-opds">What is required of the fellows and OPDs?</a></li>
+              </ul>
+            </li>
+            <li><a href="#why-the-disability-justice-project-now">Why the Disability Justice Project now?</a></li>
+            <li><a href="#accessibility-statement">Accessibility statement</a></li>
+            <li><a href="#mentors">Mentors</a></li>
+            <li><a href="#staff">Staff</a></li>
+          </ul>
+        </nav>
         <?php the_content(); ?>
       </div>
     </div>
 
-    <p class="l-landing__section l-landing__topic c-content__topic">
+    <p id="mentors" class="l-landing__section l-landing__topic c-content__topic">
       Our team
     </p>
     <h2 class="l-landing__section l-landing__heading help c-heading__large">
@@ -46,7 +62,7 @@
         <?php get_template_part('mentor'); ?>
     <?php endwhile; ?>
 
-    <h2 class="l-landing__section l-landing__heading help c-heading__large">
+    <h2 id="staff" class="l-landing__section l-landing__heading help c-heading__large">
       Staff
     </h2>
 
