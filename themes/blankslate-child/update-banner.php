20b004bb0888994f41165b70e9614b4b820ee583
Eric Bailey <eric.w.bailey@gmail.com>
2021-09-05 14:06:41 -0400

    Remove unused partial

--- themes/blankslate-child/update-banner.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! empty($new_video)) : ?>
-  <div class="c-update-banner">
-    <h1 id="title" class="c-update-banner__message">
-      Documentaries <span class="u-color-text-gold">by</span> and <span class="u-color-text-gold">about</span> persons with disabilities
-    </h1>
-  </div>
-<?php endif; ?>
