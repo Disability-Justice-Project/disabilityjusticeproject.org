9249bd5cef2798cfa1a65913a44058a837ddca8c
Eric Bailey <eric.w.bailey@gmail.com>
2021-09-05 20:19:21 -0400

    Show Fellows and 2021 cohorts

--- themes/blankslate-child/template-filmmakers.php
+++ themes/blankslate-child/template-filmmakers.php
@@ -20,13 +20,28 @@
     <h2 id="2021" class="l-landing__heading c-heading__medium">
       2021
     </h2>
-
     <?php
       $args = array(
-        'category_name' => 'filmmaker',
-        'orderby'       => '$order_by',
-        'order'         => 'DSC',
-        'no_found_rows' => true
+        'category_name' => '2021',
+        'orderby' => '$order_by',
+        'order'   => 'ASC',
+        'no_found_rows' => true,
+      );
+      $query = new WP_Query($args);
+
+      while($query -> have_posts()) : $query -> the_post(); ?>
+        <?php get_template_part('filmmaker'); ?>
+    <?php endwhile; ?>
+
+    <h2 id="fellows" class="l-landing__heading c-heading__medium">
+      Fellows
+    </h2>
+    <?php
+      $args = array(
+        'category_name' => 'fellow',
+        'orderby' => '$order_by',
+        'order'   => 'ASC',
+        'no_found_rows' => true,
       );
       $query = new WP_Query($args);
 
